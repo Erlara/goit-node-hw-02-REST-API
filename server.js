663c35cc36d1f3b23703703f097726e8646bcf3f
Larisa Melnichenko <erlara@ukr.net>
2023-05-22 13:25:47 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+//const { DB_HOST, PORT = 3000 } = process.env;
+
+const DB_HOST =
+  "mongodb+srv://Larisa:vt5wHHg9zg1Ee1rN@cluster0.elzyxvf.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    proccess.exit(1);
+  });
