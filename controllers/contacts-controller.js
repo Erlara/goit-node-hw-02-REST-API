abf7c58e4c51707d0498212670e1f08c55420601
Larisa Melnichenko <erlara@ukr.net>
2023-06-02 12:12:00 +0300

    hw5 is done

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -34,7 +34,7 @@ const removeContact = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndRemove(id);
   if (!result) {
-    throw HttpError(404, `Movie with ${id} not found`);
+    throw HttpError(404, `Contact with ${id} not found`);
   }
   res.json({
     message: "Delete success",
